bd46f4ea3d90c4ea1e5256df24f4933e20ba2d44
Hung Nguyen <hunngu@itkonsult.int>
2017-02-10 12:54:16 +0100

    Initial commit for membership

---
+++ test/sample.js
@@ -0,0 +1,12 @@
+/**
+ * http://usejsdoc.org/
+ */
+var assert = require('assert');
+
+describe("Describe something", function(){
+	describe("The way it smells", function() {
+		it("it is purge", function() {
+			assert.ok(true);
+		})
+	});
+});
